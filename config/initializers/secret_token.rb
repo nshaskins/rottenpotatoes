6bb773e29f32fc248ed4c1d4a4ec1cf009085100
neal haskins <'neal.haskins@gmail.com'>
2012-06-07 23:50:09 -0600

    initial load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hw2Rottenpotatoes::Application.config.secret_token = '227af42a81fc55fabc7a6a8b6c32e1c39139cd7b4aeb7a3cda810525bd5130176b76ae2be2fda538420e106b7417a2c2be7d41f2a3ae1cbfa97d02a4e0dcd789'
